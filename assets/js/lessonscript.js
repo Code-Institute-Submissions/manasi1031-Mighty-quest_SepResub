f11246f6b4638f8fda6c45a412b0000d3e70d4fc
Manasi Paradkar <paradkar.manasi@gmail.com>
2021-06-21 15:21:07 +0000

    Add HTML and CSS style to lesson page with collapsable tabs

--- assets/js/lessonscript.js
+++ assets/js/lessonscript.js
@@ -1,2 +1,16 @@
 /*jshint esversion: 6 */
 
+var tab = document.getElementsByClassName("lesson");
+var x;
+
+for (x = 0; x < tab.length; x++) {
+  tab[x].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
