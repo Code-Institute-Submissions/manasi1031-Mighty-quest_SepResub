829a93a60ae59a3f9c6e88741e9cecca9ddcd6b2
Manasi Paradkar <paradkar.manasi@gmail.com>
2021-06-22 13:40:12 +0000

    Add subject tab view javascript for quiz page

---
+++ assets/js/quizscript.js
@@ -0,0 +1,22 @@
+/*jshint esversion: 6 */
+
+/** Function to view quiz by subject 
+ * This should initiate the subject view only
+*/
+function openQuiz(event, subject) {
+    let i;
+    let quizData = document.getElementsByClassName("quiz-data");
+    for (i = 0; i < quizData.length; i++) {
+        quizData[i].style.display = "none";
+    }
+    let quizLinks = document.getElementsByClassName("quiz-links");
+    for (i = 0; i < quizLinks.length; i++) {
+        quizLinks[i].className = quizLinks[i].className.replace(" active", "");
+    }
+    document.getElementById(subject).style.display = "block";
+    event.currentTarget.className += " active";
+}
+
+/**
+ * Quiz Data 
+ */
