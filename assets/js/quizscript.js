4b39b58c3f9c8ddf2aec191cf5a0999d47a3bd73
Manasi Paradkar <paradkar.manasi@gmail.com>
2021-06-23 16:14:39 +0000

    Add buildQuiz javascript function for Science quiz

--- assets/js/quizscript.js
+++ assets/js/quizscript.js
@@ -26,63 +26,11 @@ function openQuiz(event, subject) {
 (function(){
     // Set up functions for quiz
     function buildQuiz(){
-        //variable to show output
-        let output = [];
-        // for each question asked
-        myQuestions.forEach (
-            (currentQuestion, questionNumber) => {
-                // variable for each answer
-                let answers = [];
-                for(letter in currentQuestion.answers) {
-                    // ...add a checkbox
-                    answers.push(
-                    `<label>
-                    <input type="checkbox" name="question${questionNumber}" value="${letter}">
-                    ${letter} :
-                    ${currentQuestion.answers[letter]}
-                    </label>`
-                    );
-                }
-                // add this question and its answers to the output
-                output.push(
-                    `<div class="question"> ${currentQuestion.question} </div>
-                    <div class="answers"> ${answers.join('')} </div>`
-                );
-            }
-        );
-        // finally combine our output list into one string of HTML and put it on the page
-        quizBox.innerHTML = output.join('');
+
     }
 
     function showResults(){
-        // gather answer box from quiz
-        let answerBoxes = quizBox.querySelectorAll('.answers');
-        // keep track of user answers
-        let correctAnswer = 0;
-        // for each question...
-        myQuestions.forEach( 
-            (currentQuestion, questionNumber) => {
-                // find selected answer
-                let answerBox = answerBoxes[questionNumber];
-                const selector = `input[name=question${questionNumber}]:checked`;
-                const userAnswer = (answerBox.querySelector(selector) || {}).value;
-
-        // if answer is correct
-        if(userAnswer === currentQuestion.correctAnswer){
-            // add to the number of correct answers
-            correctAnswer++;
-            // color the answers green
-            answerBoxes[questionNumber].style.color = 'lightgreen';
-        }
-            // if answer is wrong or blank
-        else{
-            // color the answers red
-            answerBoxes[questionNumber].style.color = 'red';
-        }
-    });
-
-    // show number of correct answers out of total
-    resultsBox.innerHTML = `${correctAnswer} out of ${myQuestions.length}`;
+ 
     }
 
     // elements for variables
@@ -90,63 +38,8 @@ function openQuiz(event, subject) {
     let resultsBox = document.getElementById('results');
     let submitBtn = document.getElementById('submit');
     // Set of questions
-        let myQuestions = [
-            {
-                question: "Eating what is important for your health?",
-                answers: {
-                    a: "Sugars",
-                    b: "Carbohydrates",
-                    c: "A balanced diet"
-                },
-                correctAnswer: "c"
-            },
-            {
-                question: "How many food groups are there in a balanced diet?",
-                answers: {
-                    a: "Five",
-                    b: "Six",
-                    c: "Seven"
-                },
-                correctAnswer: "a"
-            },
-            {
-                question: "What are building blocks for the body because they help it grow and repair itself?",
-                answers: {
-                    a: "Carbohydrates",
-                    b: "Protein",
-                    c: "Fruits and Vegetables",
-                },
-                correctAnswer: "b"
-            },
-            {
-                question: "Which products are important because they keep your bones and teeth healthy?",
-                answers: {
-                    a: "Dairy Products",
-                    b: "Protein",
-                    c: "Carbohydrates",
-                },
-                correctAnswer: "a"
-            },
-            {
-                question: "What are full of health-boosting vitamins, antioxidants and fibre?",
-                answers: {
-                    a: "Dairy Products",
-                    b: "Fruits and Vegetables",
-                    c: "Carbohydrates",
-                },
-                correctAnswer: "b"
-            },
-            {
-                question: "Too much of which food group can damage your teeth or make you gain weight?",
-                answers: {
-                    a: "Dairy Products",
-                    b: "Fruits and Vegetables",
-                    c: "Fats and Sugars",
-                },
-                correctAnswer: "c"
-            },
-        ];
-
+    let myQuestions 
+    
     // display quiz 
     buildQuiz();
 
