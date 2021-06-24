9d81bb344d373c333a40461ec98956ac13682eed
Manasi Paradkar <paradkar.manasi@gmail.com>
2021-06-24 11:04:06 +0000

    Contact page HTML indentation and update javascript to fix bug

--- assets/js/contactscript.js
+++ assets/js/contactscript.js
@@ -1,6 +1,17 @@
 /*jshint esversion: 6 */
 
-/* Function called to confirm that message has been submitted to sender */
-function myFunction() {
+/* Function called to confirm that message has been submitted to sender and gives an alert*/
+function myFunction(){
+  // Define variables
+  let name = document.getElementById("fname").value;
+  var email = document.getElementById("email").value;
+  var message = document.getElementsByClassName("large").value;
+  // conditional statement to return alert
+  if( name ==='' || email ==='' || message ===''){
+    alert("Please fill in all fields...!!!!!!");
+    return false;
+  }else{
     alert("Thank you for your feedback.");
+    return true;
   }
+}
