8cf9fb3027e8b803450636f770ebc6e18dddedc0
Manasi Paradkar <paradkar.manasi@gmail.com>
2021-06-25 13:12:31 +0000

    Declare variables for memory javascript file

--- assets/js/memoryscript.js
+++ assets/js/memoryscript.js
@@ -1,1 +1,24 @@
 /*jshint esversion: 6 */
+
+// cards array holds all cards
+let card = document.getElementsByClassName("card");
+let cards = [...card];
+
+// deck of all cards in game
+let deck = document.getElementById("card-deck");
+
+// declaring move variable
+let moves = 0;
+let counter = document.querySelector(".moves");
+
+// declaring variable of matchedCards
+let matchedCard = document.getElementsByClassName("match");
+
+// close icon in popup modal
+let closeicon = document.querySelector(".close");
+
+// declare popup modal
+let modal = document.getElementById("popup1")
+
+// array for opened cards
+let openedCards = [];
