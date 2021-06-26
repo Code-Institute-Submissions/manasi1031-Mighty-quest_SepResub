a5c3b9682764b345766382702b89290a17572f53
Manasi Paradkar <paradkar.manasi@gmail.com>
2021-06-26 06:17:04 +0000

    memory.js: Add player moves and game timer function

--- assets/js/memoryscript.js
+++ assets/js/memoryscript.js
@@ -132,3 +132,35 @@ function enable(){
         }
     });
 }
+
+// Ccount player's moves
+function moveCounter(){
+    moves++;
+    counter.innerHTML = moves;
+    //start timer on first click
+    if(moves == 1){
+        second = 0;
+        minute = 0; 
+        hour = 0;
+        startTimer();
+    }
+}
+
+// Game timer
+var second = 0, minute = 0; hour = 0;
+var timer = document.querySelector(".timer");
+var interval;
+function startTimer(){
+    interval = setInterval(function(){
+        timer.innerHTML = minute+"mins "+second+"secs";
+        second++;
+        if(second == 60){
+            minute++;
+            second=0;
+        }
+        if(minute == 60){
+            hour++;
+            minute = 0;
+        }
+    },1000);
+}
