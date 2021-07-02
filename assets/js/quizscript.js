d56d6ee9a7f5f0cb7330db797fe55991d80eed8d
Manasi Paradkar <paradkar.manasi@gmail.com>
2021-07-02 13:17:01 +0000

    Add CSS responsiveness for Science quiz

--- assets/js/quizscript.js
+++ assets/js/quizscript.js
@@ -22,71 +22,41 @@ function openQuiz(event, subject) {
 // creating an array and passing the number, questions, options, and answers
 let questions = [
     {
-    numb: 1,
-    question: "Eating what is important for your health?",
-    answer: "A Balanced Diet",
-    options: [
-        "Sugars",
-        "Carbohydrates",
-        "A Balanced Diet",
-        "Junk Food"
-        ]
+        numb: 1,
+        question: "Eating what is important for your health?",
+        answer: "A Balanced Diet",
+        options: ["Sugars", "Carbohydrates", "A Balanced Diet", "Junk Food"]
     },
     {
-    numb: 2,
-    question: "How many food groups are there in a balanced diet?",
-    answer: "Five",
-    options: [
-        "Five",
-        "Six",
-        "Seven",
-        "Ten"
-        ]
+        numb: 2,
+        question: "How many food groups are there in a balanced diet?",
+        answer: "Five",
+        options: [ "Five", "Six", "Seven", "Ten" ]
     },
     {
-    numb: 3,
-    question: "What are building blocks for the body because they help it grow and repair itself?",
-    answer: "Protein",
-    options: [
-        "Carbohydrates",
-        "Protein",
-        "Fruits and Vegetables",
-        "Dairy Products"
-        ]
+        numb: 3,
+        question: "What are building blocks for the body because they help it grow and repair itself?",
+        answer: "Protein",
+        options: [ "Carbohydrates", "Protein", "Fruits and Vegetables", "Dairy Products"]
     },
     {
-    numb: 4,
-    question: "Which products are important because they keep your bones and teeth healthy?",
-    answer: "Dairy Products",
-    options: [
-        "Protein",
-        "Carbohydrates",
-        "Dairy Products",
-        "Fruits and Vegetables"
-        ]
+        numb: 4,
+        question: "Which products are important because they keep your bones and teeth healthy?",
+        answer: "Dairy Products",
+        options: [ "Protein", "Carbohydrates", "Dairy Products", "Fruits and Vegetables" ]
     },
     {
-    numb: 5,
-    question: "What are full of health-boosting vitamins, antioxidants and fibre?",
-    answer: "Fruits and Vegetables",
-    options: [
-        "Dairy Products",
-        "Carbohydrates",
-        "Fruits and Vegetables",
-        "Protein"
-        ]
+        numb: 5,
+        question: "What are full of health-boosting vitamins, antioxidants and fibre?",
+        answer: "Fruits and Vegetables",
+        options: [ "Dairy Products", "Carbohydrates", "Fruits and Vegetables", "Protein" ]
     },
     {
         numb: 6,
         question: "Too much of which food group can damage your teeth or make you gain weight?",
         answer: "Fats and Sugars",
-        options: [
-            "Dairy Products",
-            "Carbohydrates",
-            "Fruits and Vegetables",
-            "Fats and Sugars"
-            ]
-        }
+        options: [ "Dairy Products", "Carbohydrates", "Fruits and Vegetables", "Fats and Sugars" ]
+    }
 ];
 
 //Declare variables for Science Quiz
