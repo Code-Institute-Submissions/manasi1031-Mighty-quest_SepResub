b069943fe6ededb6da96979497cac59c2936f3bc
Manasi Paradkar <paradkar.manasi@gmail.com>
2021-07-07 16:31:53 +0000

    Amend Quiz page HTML/CSS/JS to view resources differently

--- assets/js/quizscript.js
+++ assets/js/quizscript.js
@@ -282,15 +282,30 @@ function questionCounter(index){
     bottom_ques_counter.innerHTML = totalQueCounTag;  //adding new span tag inside bottom_ques_counter
 }
 
-// Function to open and close pop up resource box for Geography and Maths 
-$(window).load(function () {
-    $(".trigger_popup").click(function(){
-       $('.hover_bkgr').show();
-    });
-    $('.hover_bkgr').click(function(){
-        $('.hover_bkgr').hide();
-    });
-    $('.popupCloseButton').click(function(){
-        $('.hover_bkgr').hide();
-    });
+// Open/close resources in a pop up box
+
+// select the open-btn button
+let openBtn = document.getElementById('open-btn');
+// select the modal-background
+let modalBackground = document.getElementById('modal-background');
+// select the close-btn 
+let closeBtn = document.getElementById('close-btn');
+
+// shows the modal when the user clicks open-btn
+openBtn.addEventListener('click', function() {
+  modalBackground.style.display = 'block';
+});
+
+// hides the modal when the user clicks close-btn
+closeBtn.addEventListener('click', function() {
+  modalBackground.style.display = 'none';
+});
+
+// hides the modal when the user clicks outside the modal
+window.addEventListener('click', function(event) {
+  // check if the event happened on the modal-background
+  if (event.target === modalBackground) {
+    // hides the modal
+    modalBackground.style.display = 'none';
+  }
 });
