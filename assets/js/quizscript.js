88bd03672c8c02d951eda938a302c9d42e8e3526
Manasi Paradkar <paradkar.manasi@gmail.com>
2021-07-08 21:44:43 +0000

    Add CSS and JS sections for Maths resources modal

--- assets/js/quizscript.js
+++ assets/js/quizscript.js
@@ -309,3 +309,53 @@ window.addEventListener('click', function(event) {
     modalBackground.style.display = 'none';
   }
 });
+
+
+// Maths section Resources Modal or pop up section
+// Get the modal and define variable
+var modal = document.getElementById('myModal');
+    
+// Get the image and insert it inside the modal and define variables
+// use its "alt" text as a caption
+var img1 = document.getElementById('myImg1');
+var img2 = document.getElementById('myImg2');
+var img3 = document.getElementById('myImg3');
+var img4 = document.getElementById('myImg4');
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+
+img1.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    modalImg.alt = this.alt;
+    captionText.innerHTML = this.alt;
+}
+
+img2.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    modalImg.alt = this.alt;
+    captionText.innerHTML = this.alt;
+}
+
+img3.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    modalImg.alt = this.alt;
+    captionText.innerHTML = this.alt;
+}
+
+img4.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    modalImg.alt = this.alt;
+    captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("closemodal")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+  modal.style.display = "none";
+}
