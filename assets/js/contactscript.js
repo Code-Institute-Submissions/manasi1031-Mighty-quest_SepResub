749680fc7406055d645c8ffde6cb0ff7d0764533
Manasi Paradkar <paradkar.manasi@gmail.com>
2021-08-25 19:03:20 +0000

    Fix <li> type attribute to data-type and add CSS style on memory JS and HTML

--- assets/js/contactscript.js
+++ assets/js/contactscript.js
@@ -3,7 +3,7 @@
 /* Function called to confirm that message has been submitted to sender and gives an alert*/
 /* Data is not recorded anywhere or populated*/
 function submitForm(){
-  document.getElementById("contactus-form").submit();
+  document.getElementById("submit-button").submit();
   // Define variables
   let name = document.getElementById("fname").value;
   var email = document.getElementById("email").value;
