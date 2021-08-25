aab278bbbc635b512700157dc3104effdb682cc9
Manasi Paradkar <paradkar.manasi@gmail.com>
2021-08-25 19:21:39 +0000

    Remove CSS for memory page card list as cards not matching correctly

--- assets/js/contactscript.js
+++ assets/js/contactscript.js
@@ -3,7 +3,6 @@
 /* Function called to confirm that message has been submitted to sender and gives an alert*/
 /* Data is not recorded anywhere or populated*/
 function submitForm(){
-  document.getElementById("submit-button").submit();
   // Define variables
   let name = document.getElementById("fname").value;
   var email = document.getElementById("email").value;
